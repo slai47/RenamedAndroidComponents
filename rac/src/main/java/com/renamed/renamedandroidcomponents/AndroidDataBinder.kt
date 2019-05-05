a1ee4b3e46d2a91dee5f092b7dd7f9dfa0283a0f
Harrison Frede <harrisonfrede@gmail.com>
2019-05-05 12:04:55 -0400

    Getting the general setup of DataBinder and DataStore

--- rac/src/main/java/com/renamed/renamedandroidcomponents/AndroidDataBinder.kt
+++ rac/src/main/java/com/renamed/renamedandroidcomponents/AndroidDataBinder.kt
@@ -1,26 +1,39 @@
 package com.renamed.renamedandroidcomponents
 
-import androidx.lifecycle.AndroidViewModel
 import android.app.Application
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.*
 
 open class AndroidDataBinder(application: Application) : AndroidViewModel(application)
 
 open class DataBinders{
 
-    fun of(frag : Fragment): ViewModelProvider {
-        return ViewModelProviders.of(frag)
+    fun of(frag : Fragment): DataBinderProvider {
+        return ViewModelProviders.of(frag) as DataBinderProvider
     }
-    fun of(frag : FragmentActivity): ViewModelProvider {
-        return ViewModelProviders.of(frag)
+    fun of(frag : FragmentActivity): DataBinderProvider {
+        return ViewModelProviders.of(frag) as DataBinderProvider
     }
-    fun of(frag : Fragment, factory : ViewModelProvider.Factory): ViewModelProvider {
-        return ViewModelProviders.of(frag, factory)
+    fun of(frag : Fragment, factory : ViewModelProvider.Factory): DataBinderProvider {
+        return ViewModelProviders.of(frag, factory) as DataBinderProvider
     }
-    fun of(frag : FragmentActivity, factory : ViewModelProvider.Factory): ViewModelProvider {
-        return ViewModelProviders.of(frag, factory)
+    fun of(frag : FragmentActivity, factory : ViewModelProvider.Factory): DataBinderProvider {
+        return ViewModelProviders.of(frag, factory) as DataBinderProvider
+    }
+}
+
+open class DataBinderProvider : ViewModelProvider {
+
+    constructor(owner : ViewModelStoreOwner, factory : Factory) : super(owner, factory)
+
+    constructor(store : ViewModelStore, factory : Factory) : super(store, factory)
+
+    fun <T : AndroidDataBinder> get(modelClass : Class<T>) : AndroidDataBinder {
+        return super.get(modelClass)
+    }
+
+    fun <T : AndroidDataBinder> get(key : String, modelClass : Class<T>) : AndroidDataBinder {
+        return super.get(key, modelClass)
     }
 }
--- rac/src/main/java/com/renamed/renamedandroidcomponents/AndroidDataStore.kt
+++ rac/src/main/java/com/renamed/renamedandroidcomponents/AndroidDataStore.kt
@@ -3,24 +3,37 @@ package com.renamed.renamedandroidcomponents
 import android.app.Application
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.*
 
 open class AndroidDataStore(application: Application) : AndroidViewModel(application)
 
 open class DataStores{
 
-    fun of(frag : Fragment): ViewModelProvider {
-        return ViewModelProviders.of(frag)
+    fun of(frag : Fragment): DataStoreProvider {
+        return ViewModelProviders.of(frag) as DataStoreProvider
     }
-    fun of(frag : FragmentActivity): ViewModelProvider {
-        return ViewModelProviders.of(frag)
+    fun of(frag : FragmentActivity): DataStoreProvider {
+        return ViewModelProviders.of(frag) as DataStoreProvider
     }
-    fun of(frag : Fragment, factory : ViewModelProvider.Factory): ViewModelProvider {
-        return ViewModelProviders.of(frag, factory)
+    fun of(frag : Fragment, factory : ViewModelProvider.Factory): DataStoreProvider {
+        return ViewModelProviders.of(frag, factory) as DataStoreProvider
     }
-    fun of(frag : FragmentActivity, factory : ViewModelProvider.Factory): ViewModelProvider {
-        return ViewModelProviders.of(frag, factory)
+    fun of(frag : FragmentActivity, factory : ViewModelProvider.Factory): DataStoreProvider {
+        return ViewModelProviders.of(frag, factory) as DataStoreProvider
+    }
+}
+
+open class DataStoreProvider : ViewModelProvider {
+
+    constructor(owner : ViewModelStoreOwner, factory : Factory) : super(owner, factory)
+
+    constructor(store : ViewModelStore, factory : Factory) : super(store, factory)
+
+    fun <T : AndroidDataStore> get(modelClass : Class<T>) : AndroidDataStore {
+        return super.get(modelClass)
+    }
+
+    fun <T : AndroidDataStore> get(key : String, modelClass : Class<T>) : AndroidDataStore {
+        return super.get(key, modelClass)
     }
 }
